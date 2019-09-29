e8392cecafc4a9d29978ec9e934cfbf12b6be23e
Vince Varga <vargavince91@gmail.com>
2019-09-29 16:07:59 +0200

    📖 Add empty example
    Executed in the example folder:
    
    flutter create --template app --org dev.dartside.pub.cooler_themes_example --project-name cooler_themes_example .

---
+++ example/android/app/src/main/kotlin/dev/dartside/pub/cooler_themes_example/cooler_themes_example/MainActivity.kt
@@ -0,0 +1,13 @@
+package dev.dartside.pub.cooler_themes_example.cooler_themes_example
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
